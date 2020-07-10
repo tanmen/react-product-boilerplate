b6c442272de0123d319a892309a1fb85a4ab687f
tanmen <yt.prog@gmail.com>
2020-07-10 17:31:19 +0900

    Change lint & Split setting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  userEvent.click(screen.getByText(/learn react/i));
 });
