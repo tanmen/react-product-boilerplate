276b48fbfa4b7f60e51d0491d17efe0c7328645a
tanmen <yt.prog@gmail.com>
2020-07-10 18:35:19 +0900

    Add emotion

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const {getByText} = render(<App/>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
   userEvent.click(screen.getByText(/learn react/i));
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Base>
+    <Header className="App-header">
+      <Logo src={logo} alt="logo"/>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <Link
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </Link>
+    </Header>
+  </Base>
+);
 
 export default App;
+
+const Base = styled.div`
+text-align: center;
+`;
+
+const Header = styled.header`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+`;
+
+const Logo = styled.img`
+height: 40vmin;
+pointer-events: none;
+
+@media (prefers-reduced-motion: no-preference) {
+  animation: App-logo-spin infinite 20s linear;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+`;
+
+const Link = styled.a`
+color: #61dafb;
+`;
